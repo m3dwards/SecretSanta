70f4a141ca4357b5a559b3c459a16849eb4f2302
Max Edwards <maxedwar@thoughtworks.com>
2015-11-10 23:02:56 +0000

    Changed main namespace for heroku

--- secret-santa-web/project.clj
+++ secret-santa-web/project.clj
@@ -7,6 +7,7 @@
                  [ring/ring-defaults "0.1.5"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler secret-santa-web.handler/app}
-  :profiles
+  :profiles {:production {:env {:production true}}}
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}}
+  :uberjar-name "secret-santa.jar")
