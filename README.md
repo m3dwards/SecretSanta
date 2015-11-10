e4755a68b18c42367bf170915a5d59869c92f207
Max Edwards <maxedwar@thoughtworks.com>
2015-11-10 21:57:33 +0000

    added default comojure app

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SecretSanta
+
+Install Postgres 9.4 or above which on a Mac is 'brew install postgresql'
+
+Optionally 'initdb db' in a directory locally to create a local postgres rather than use the default one. Run it using 'postgres -D db'.
+
+Create a database 'createdb secret-santa'
+
---
+++ secret-santa-web/test/secret_santa_web/handler_test.clj
@@ -0,0 +1,14 @@
+(ns secret-santa-web.handler-test
+  (:require [clojure.test :refer :all]
+            [ring.mock.request :as mock]
+            [secret-santa-web.handler :refer :all]))
+
+(deftest test-app
+  (testing "main route"
+    (let [response (app (mock/request :get "/"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "Hello World"))))
+
+  (testing "not-found route"
+    (let [response (app (mock/request :get "/invalid"))]
+      (is (= (:status response) 404)))))
