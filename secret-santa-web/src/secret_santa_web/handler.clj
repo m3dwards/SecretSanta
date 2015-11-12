cbd3dca8e3f611dc24d1e3f13505f7ff875c7803
Max Edwards <maxedwar@thoughtworks.com>
2015-11-12 22:17:50 +0000

    Added static file serving to clojure

--- secret-santa-web/src/secret_santa_web/handler.clj
+++ secret-santa-web/src/secret_santa_web/handler.clj
@@ -2,16 +2,45 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :refer [site]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.content-type :refer [wrap-content-type]]
+            [ring.middleware.not-modified :refer [wrap-not-modified]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.util.response :refer [resource-response content-type]]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] (content-type (resource-response "index.html" {:root "public"}) "text/html"))
+  (GET "/backend" [] "Hello backend")
+  (GET "/broken" [] (/ 1 0))
+  (route/not-found "<html><body><img src='/img/404.png' style='max-width:100%'/></body></html>")
+  )
+
+(defn wrap-exception [f]
+  (fn [request]
+    (try (f request)
+      (catch Exception e
+         {:status 500
+          :body "<html><body><img src='img/500.png' style='max-width:100%'/></body></html>"}))))
+
+(defn ignore-trailing-slash
+  [handler]
+  (fn [request]
+    (let [uri (:uri request)]
+      (handler (assoc request :uri (if (and (not (= "/" uri))
+                                            (.endsWith uri "/"))
+                                     (subs uri 0 (dec (count uri)))
+                                     uri))))))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+    (ignore-trailing-slash)
+    (wrap-defaults site-defaults)
+    (wrap-content-type)
+    (wrap-not-modified)
+    (wrap-exception)))
+
 
 (defn -main [& [port]]
     (let [port (Integer. (or port (env :port) 5000))]
