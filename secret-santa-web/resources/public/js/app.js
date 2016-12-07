ec309af0f4e983e2f35e3d8bfc4550043fb695de
Aaron Rhodes <aaroncraigrhodes@googlemail.com>
2016-12-07 18:08:22 +0000

    Added UI for adding users to an event

--- secret-santa-web/resources/public/js/app.js
+++ secret-santa-web/resources/public/js/app.js
@@ -191,7 +191,7 @@ app.controller('appController', ['$rootScope', '$scope', '$route', '$location', 
 		}
 	}
 ]);
-app.controller('editEventController', function ($scope, $routeParams, event, preferences, dates, venues, $location) {
+app.controller('editEventController', function ($scope, $routeParams, event, preferences, dates, venues, eventUsers, $location) {
 
     var self = this;
     self.eventId = $routeParams.id;
@@ -208,10 +208,13 @@ app.controller('editEventController', function ($scope, $routeParams, event, pre
     self.event = { name:null, date:null };
     self.addedDates = [];
     self.addedVenues = [];
+    self.addedAttendees = [];
     self.name = null;
 
     self.newDate = moment().format('d MMMM YYYY');
     self.newVenue = null;
+    self.newAttendee = null;
+    self.newAttendeeValid = true;
 
     if (!self.eventId)
     {
@@ -261,6 +264,69 @@ app.controller('editEventController', function ($scope, $routeParams, event, pre
         return false;
     }
 
+    self.addAttendee = function(attendee){
+        self.newAttendeeValid = true;
+
+        if (attendee.indexOf(';') > 0)
+        {
+            var splits = attendee.split(';');
+
+            for (var i = 0; i < splits.length; i++){
+                if (!validateEmail(splits[i]))
+                {
+                    self.newAttendeeValid = false;
+                }
+            }
+
+            for (var i = 0; i < splits.length; i++)
+            {
+                self.addedAttendees.push(attendee.trim());
+            }
+
+            return false;
+        }
+        else if (attendee.indexOf(',') > 0)
+        {
+            var splits = attendee.split(',');
+
+            for (var i = 0; i < splits.length; i++){
+                if (!validateEmail(splits[i]))
+                {
+                    self.newAttendeeValid = false;
+                }
+            }
+
+            for (var i = 0; i < splits.length; i++)
+            {
+                self.addedAttendees.push(attendee.trim());
+            }
+
+            return false;
+        }
+
+        if (!validateEmail(attendee)) {
+            self.newAttendeeValid = false;
+            return false;
+        }
+
+        self.addedAttendees.push(attendee);
+
+        return false;
+    }
+
+    self.validateNewAttendee = function(){
+        if (validateEmail(self.newAttendee))
+        {
+            self.newAttendeeValid = true;
+        }
+    }
+
+    self.removeAttendee = function(attendee){
+        self.addedAttendees.pop(attendee);
+
+        return false;
+    }
+
     self.saveEvent = function(){
         if (self.creating){
             event.save({
@@ -284,8 +350,14 @@ app.controller('editEventController', function ($scope, $routeParams, event, pre
         dates.save({id: eventId}, {dates: converted});
 
         venues.save({id:eventId}, {venues:self.addedVenues});
+
+        eventUsers.save({id:eventId}, self.addedAttendees)
     }
 
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 
     //$location.path( "/event/:id/edit" );
 });
--- secret-santa-web/src/secret_santa_client/js/editEventController.js
+++ secret-santa-web/src/secret_santa_client/js/editEventController.js
@@ -1,4 +1,4 @@
-app.controller('editEventController', function ($scope, $routeParams, event, preferences, dates, venues, $location) {
+app.controller('editEventController', function ($scope, $routeParams, event, preferences, dates, venues, eventUsers, $location) {
 
     var self = this;
     self.eventId = $routeParams.id;
@@ -15,10 +15,13 @@ app.controller('editEventController', function ($scope, $routeParams, event, pre
     self.event = { name:null, date:null };
     self.addedDates = [];
     self.addedVenues = [];
+    self.addedAttendees = [];
     self.name = null;
 
     self.newDate = moment().format('d MMMM YYYY');
     self.newVenue = null;
+    self.newAttendee = null;
+    self.newAttendeeValid = true;
 
     if (!self.eventId)
     {
@@ -68,6 +71,69 @@ app.controller('editEventController', function ($scope, $routeParams, event, pre
         return false;
     }
 
+    self.addAttendee = function(attendee){
+        self.newAttendeeValid = true;
+
+        if (attendee.indexOf(';') > 0)
+        {
+            var splits = attendee.split(';');
+
+            for (var i = 0; i < splits.length; i++){
+                if (!validateEmail(splits[i]))
+                {
+                    self.newAttendeeValid = false;
+                }
+            }
+
+            for (var i = 0; i < splits.length; i++)
+            {
+                self.addedAttendees.push(attendee.trim());
+            }
+
+            return false;
+        }
+        else if (attendee.indexOf(',') > 0)
+        {
+            var splits = attendee.split(',');
+
+            for (var i = 0; i < splits.length; i++){
+                if (!validateEmail(splits[i]))
+                {
+                    self.newAttendeeValid = false;
+                }
+            }
+
+            for (var i = 0; i < splits.length; i++)
+            {
+                self.addedAttendees.push(attendee.trim());
+            }
+
+            return false;
+        }
+
+        if (!validateEmail(attendee)) {
+            self.newAttendeeValid = false;
+            return false;
+        }
+
+        self.addedAttendees.push(attendee);
+
+        return false;
+    }
+
+    self.validateNewAttendee = function(){
+        if (validateEmail(self.newAttendee))
+        {
+            self.newAttendeeValid = true;
+        }
+    }
+
+    self.removeAttendee = function(attendee){
+        self.addedAttendees.pop(attendee);
+
+        return false;
+    }
+
     self.saveEvent = function(){
         if (self.creating){
             event.save({
@@ -91,8 +157,14 @@ app.controller('editEventController', function ($scope, $routeParams, event, pre
         dates.save({id: eventId}, {dates: converted});
 
         venues.save({id:eventId}, {venues:self.addedVenues});
+
+        eventUsers.save({id:eventId}, self.addedAttendees)
     }
 
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
 
     //$location.path( "/event/:id/edit" );
 });
